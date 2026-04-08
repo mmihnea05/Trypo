b08285094394e2161b4b1e28856f54ffbcb2fd12
IoanBarbaros <barbarosioan12@gmail.com>
2026-04-08 19:56:22 +0300

    Test BD

--- sample_diagram/Users.h
+++ sample_diagram/Users.h
@@ -15,6 +15,15 @@ private:
 	string address;
 
 public:
+	Users(string name,string password, string mail,string phonenumber,string birthDate, string country, string gender, string address): name(name), password(password), mail(mail), phoneNumber(phonenumber), birthDate(birthDate),country(country), gender(gender),address(address) {}
 
+	std::string getName()  { return name; }
+	std::string getPassword()  { return password; }
+	std::string getMail()  { return mail; }
+	std::string getPhone()  { return phoneNumber; }
+	std::string getBirthDate() { return birthDate; }
+	std::string getCountry()  { return country; }
+	std::string getGender()  { return gender; }
+	std::string getAddress() { return address; }
 };
 
